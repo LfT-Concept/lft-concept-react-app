38e0e5e21c5392a526da920cbe16638ff46314f0
Syed Ahmad <syed-ahmad@outlook.com>
2019-05-08 09:31:59 +0100

    Use FormData to upload images to rest api

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -106,7 +106,11 @@ class Feed extends Component {
     this.setState({
       editLoading: true
     });
-    // Set up data (with image!)
+    const formData = new FormData();
+    formData.append('title', postData.title);
+    formData.append('content', postData.content);
+    formData.append('image', postData.image);
+    
     let url = `${process.env.REACT_APP_REST_API}/feed/posts`;
     let method = 'POST';
     if (this.state.editPost) {
@@ -117,13 +121,8 @@ class Feed extends Component {
 
     fetch(url, {
       method: method,
-      headers: {
-        'Content-Type' : 'application/json'
-      },
-      body: JSON.stringify({
-        title: postData.title,
-        content: postData.content
-      })
+      // FormData() sets the correct headers
+      body: formData
     })
     .then(res => {
         if (res.status !== 200 && res.status !== 201) {
